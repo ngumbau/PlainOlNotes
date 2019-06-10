4e6a77cbcb49c882be38fad0f01f56e5ed26d8b8
Kristopher Kivutha <kkibwana@gmail.com>
2019-06-10 14:57:21 +0300

    added the data management layer dealing with sqlite database

--- app/src/main/java/android/krinye/plainolnotes/MainActivity.java
+++ app/src/main/java/android/krinye/plainolnotes/MainActivity.java
@@ -1,13 +1,125 @@
 package android.krinye.plainolnotes;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity
+implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private CursorAdapter cursorAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] from = {DbOpenHelper.NOTE_TEXT};
+        int[] to = {android.R.id.text1};
+        cursorAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1,
+                null, from, to, 0);
+        ListView list = (ListView) findViewById(android.R.id.list);
+        list.setAdapter(cursorAdapter);
+
+        getSupportLoaderManager().initLoader(0, null, this);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        switch(id){
+            case R.id.action_create_sample:
+                insertSampleData();
+                break;
+            case R.id.action_delete_all:
+                deleteAllNotes();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void deleteAllNotes() {
+        DialogInterface.OnClickListener dialogClickListener =
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int button) {
+                        if (button == DialogInterface.BUTTON_POSITIVE) {
+                            //Insert Data management code here
+                            getContentResolver().delete(
+                                    NotesProvider.CONTENT_URI, null, null
+                            );
+                            restartLoader();
+                            Toast.makeText(MainActivity.this,
+                                    getString(R.string.all_deleted),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getString(R.string.are_you_sure))
+                .setPositiveButton(getString(android.R.string.yes), dialogClickListener)
+                .setNegativeButton(getString(android.R.string.no), dialogClickListener)
+                .show();
+    }
+
+    private void insertSampleData() {
+        insertNote("Simple note");
+        insertNote("Multi-line\nnote");
+        insertNote("Very long note with a lot of text that exceeds the width of the screen");
+        restartLoader();
+    }
+
+    private void restartLoader(){
+        getSupportLoaderManager().restartLoader(0, null, this);
+    }
+    private void insertNote(String noteText) {
+        ContentValues values = new ContentValues();
+        values.put(DbOpenHelper.NOTE_TEXT, noteText);
+        Uri noteUri = getContentResolver().insert(NotesProvider.CONTENT_URI,
+                values);
+        Log.d("Main Activity", "Inserted note " + noteUri.getLastPathSegment());
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, @Nullable Bundle bundle) {
+        return new CursorLoader(this, NotesProvider.CONTENT_URI,
+                null, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+        cursorAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+        cursorAdapter.swapCursor(null);
     }
 }
---
+++ app/src/main/java/android/krinye/plainolnotes/NotesProvider.java
@@ -0,0 +1,63 @@
+package android.krinye.plainolnotes;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+
+public class NotesProvider extends ContentProvider {
+
+    public static final String AUTHORITY = "android.krinye.plainolnotes.notesprovider";
+    public static final String BASE_PATH = "notes";
+    public static final Uri CONTENT_URI =
+            Uri.parse("content://" + AUTHORITY + "/" + BASE_PATH);
+
+    // Constants to identify the requested operation.
+    private static final int NOTES = 1;
+    private static final int NOTES_ID = 2;
+
+    private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static{
+        uriMatcher.addURI(AUTHORITY, BASE_PATH, NOTES);
+        uriMatcher.addURI(AUTHORITY, BASE_PATH + "/#", NOTES_ID);
+    }
+
+    private SQLiteDatabase database;
+
+    @Override
+    public boolean onCreate() {
+        DbOpenHelper helper = new DbOpenHelper(getContext());
+        database = helper.getWritableDatabase();
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        return database.query(DbOpenHelper.TABLE_NOTES, DbOpenHelper.ALL_COLUMNS, selection, null, null, null,
+                DbOpenHelper.NOTE_CREATED + " DESC");
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        long id = database.insert(DbOpenHelper.TABLE_NOTES, null, values);
+        return Uri.parse(BASE_PATH + "/" + id);
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return database.delete(DbOpenHelper.TABLE_NOTES, selection, selectionArgs);
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return database.update(DbOpenHelper.TABLE_NOTES, values, selection, selectionArgs);
+    }
+}
